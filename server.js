26e9d673a1c7559f4654e7d470ce74f46dbca17a
Joseph526 <33866177+Joseph526@users.noreply.github.com>
2018-05-15 20:51:26 -0400

    Created models

--- server.js
+++ server.js
@@ -3,11 +3,27 @@ var app = express();
 var passport = require('passport');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var env = require('dotenv').load();
 
 // For BodyParser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: 'keyboard cat',resave: true, saveUninitialized:true})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+// Models
+var models = require("./app/models");
  
+// Sync Database
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
 app.get('/', function(req, res) {
     res.send('Welcome to Passport with Sequelize');
 });
